fd753dd16ef591eb2bef73ca743858eb9a97d1cb
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-05 13:51:52 +0300

    app component photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Photo } from './image-slide/image-slide.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bootstrapImageSlide';
+  photos: Photo[] = [
+    { id: "1", url: "https://www.camhotel.com.tr/uploads/ulkemizin-dort-bir-yanindan-guzel-manzara-fotograflari.jpg", },
+    { id: "2", url: "https://img.internethaber.com/rcman/Cw480h270q95gc/images/gallery/37266/b.jpg", },
+    { id: "3", url: "https://cdn.neredekal.com/blog/IuR/760x380/doganin-carpici-renkleriyle-muhtesem-manzaralar.jpg", },
+  ]
 }
