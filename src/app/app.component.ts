6fdd2701dc1574e0141aaca5eabbcd8dc096ffeb
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-05 13:55:14 +0300

    set isMain

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,8 @@ import { Photo } from './image-slide/image-slide.component';
 export class AppComponent {
   title = 'bootstrapImageSlide';
   photos: Photo[] = [
-    { id: "1", url: "https://www.camhotel.com.tr/uploads/ulkemizin-dort-bir-yanindan-guzel-manzara-fotograflari.jpg", },
-    { id: "2", url: "https://img.internethaber.com/rcman/Cw480h270q95gc/images/gallery/37266/b.jpg", },
-    { id: "3", url: "https://cdn.neredekal.com/blog/IuR/760x380/doganin-carpici-renkleriyle-muhtesem-manzaralar.jpg", },
+    { id: "1", isMain: true, url: "https://www.camhotel.com.tr/uploads/ulkemizin-dort-bir-yanindan-guzel-manzara-fotograflari.jpg", },
+    { id: "2", isMain: false, url: "https://img.internethaber.com/rcman/Cw480h270q95gc/images/gallery/37266/b.jpg", },
+    { id: "3", isMain: false, url: "https://cdn.neredekal.com/blog/IuR/760x380/doganin-carpici-renkleriyle-muhtesem-manzaralar.jpg", },
   ]
 }
