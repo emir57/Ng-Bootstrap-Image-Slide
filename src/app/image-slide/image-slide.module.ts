07f47ac9630b4dbdb4ebc586a009665cfc72bde6
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-05 13:37:13 +0300

    Create ImageSlide Component

--- src/app/image-slide/image-slide.module.ts
+++ src/app/image-slide/image-slide.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ImageSlideComponent } from "./image-slide.component";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ImageSlideComponent
+  ],
   imports: [
     CommonModule
   ]
